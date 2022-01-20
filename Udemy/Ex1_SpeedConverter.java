719c8d0ecca5c515f07220fbaa4070fc3f0eba25
whebat <>
2022-01-20 15:04:31 +0800

    Initial commit

---
+++ Udemy/Ex1_SpeedConverter.java
@@ -0,0 +1,77 @@
+/**
+@author whebat
+*/
+
+public class Ex1_SpeedConverter
+{
+	public static long toMilesPerHour(double kilometersPerHour)
+	{
+		if (kilometersPerHour < 0)
+			return -1;
+
+		return Math.round(kilometersPerHour / 1.609);
+	}
+
+	public static void printConversion(double kilometersPerHour)
+	{
+		if (kilometersPerHour < 0)
+			System.out.println("Invalid Value");
+
+		else {
+			long milesPerHour = toMilesPerHour(kilometersPerHour);
+			System.out.println(kilometersPerHour + " km/h = " +
+			                   milesPerHour + " mi/h ");
+		}
+	}
+}
+
+/*
+1. Write a method called toMilesPerHour that has 1 parameter of type double
+   with the name kilometersPerHour. This method needs to return the rounded
+   value of the calculation of type long.
+
+   If the parameter kilometersPerHour is less than 0, the method toMilesPerHour
+   needs to return -1 to indicate an invalid value.
+
+   Otherwise, if it is positive, calculate the value of miles per hour, round
+   it and return it. For conversion and rounding, check the notes in the text
+   below.
+
+   Examples of input/output:
+   toMilesPerHour(1.5); → should return value 1
+   toMilesPerHour(10.25); → should return value 6
+   toMilesPerHour(-5.6); → should return value -1
+   toMilesPerHour(25.42); → should return value 16
+   toMilesPerHour(75.114); → should return value 47
+
+2. Write another method called printConversion with 1 parameter of type double
+   with the name kilometersPerHour.
+
+   This method should not return anything (void) and it needs to calculate
+   milesPerHour from the kilometersPerHour parameter.
+
+   Then it needs to print a message in the format "XX km/h = YY mi/h".
+
+   XX represents the original value kilometersPerHour.
+   YY represents the rounded milesPerHour from the kilometersPerHour parameter.
+
+   If the parameter kilometersPerHour is < 0
+   then print the text "Invalid Value".
+
+   Examples of input/output:
+   printConversion(1.5); → 1.5 km/h = 1 mi/h
+   printConversion(10.25); → 10.25 km/h = 6 mi/h
+   printConversion(-5.6); → Invalid Value
+   printConversion(25.42); → 25.42 km/h = 16 mi/h
+   printConversion(75.114); → 75.114 km/h = 47 mi/h
+
+   Use method Math.round to round the number of calculated mi/h (double).
+   The method round returns long.
+
+TIP:  In the method printConversion,
+      call the method toMilesPerHour instead of duplicating the code.
+
+NOTE: All methods should be defined as public static.
+      1 mile per hour is 1.609 kilometers per hour.
+      Do not add a main method to the solution code.
+*/
---
+++ Udemy/Ex2_MegaBytesConverter.java
@@ -0,0 +1,56 @@
+/**
+@author whebat
+*/
+
+public class Ex2_MegaBytesConverter
+{
+	public static void printMegaBytesAndKiloBytes(int kiloBytes)
+	{
+		if (kiloBytes < 0)
+			System.out.println("Invalid Value");
+		else {
+
+		int remainingkiloBytes = kiloBytes % 1024;
+		int megabytes = (kiloBytes - remainingkiloBytes) / 1024;
+
+		System.out.println(kiloBytes + " KB = " +
+		                   megabytes + " MB and " +
+		                   remainingkiloBytes + " KB");
+		}
+	}
+}
+
+/*
+Write a method called printMegaBytesAndKiloBytes that has
+1 parameter of type int with the name kiloBytes.
+
+The method should not return anything (void) and it needs to calculate
+the megabytes and remaining kilobytes from the kilobytes parameter.
+
+Then it needs to print a message in the format "XX KB = YY MB and ZZ KB".
+
+XX represents the original value kiloBytes.
+YY represents the calculated megabytes.
+ZZ represents the calculated remaining kilobytes.
+
+For example, when the parameter kiloBytes is 2500
+it needs to print "2500 KB = 2 MB and 452 KB"
+
+If the parameter kiloBytes is less than 0
+then print the text "Invalid Value".
+
+EXAMPLE INPUT/OUTPUT:
+
+     printMegaBytesAndKiloBytes(2500); → "2500 KB = 2 MB and 452 KB"
+     printMegaBytesAndKiloBytes(-1024); → "Invalid Value"
+     printMegaBytesAndKiloBytes(5000); → "5000 KB = 4 MB and 904 KB"
+
+TIP: Be extremely careful about spaces in the printed message.
+     Use the remainder operator
+     1 MB = 1024 KB
+
+NOTE: Do not set kilobytes parameter value inside your method.
+      The solution will not be accepted if there are extra spaces.
+      The method needs to be defined as public static.
+      Do not add a main method to solution code.
+*/
